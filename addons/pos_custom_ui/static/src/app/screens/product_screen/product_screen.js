d33d7398b7ffa095270d2b41b98e6d2bc8dee13d
xiaohao0576 <xiaohao0576@gmail.com>
2026-06-16 16:05:36 +0700

    增加产品详情长按时间

--- addons/pos_custom_ui/static/src/app/screens/product_screen/product_screen.js
+++ addons/pos_custom_ui/static/src/app/screens/product_screen/product_screen.js
@@ -1,7 +1,47 @@
 import { patch } from "@web/core/utils/patch";
 import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
+import { debounce } from "@web/core/utils/timing";
+
+const PRODUCT_INFO_LONG_PRESS_DURATION = 5000;
+
+function createProductInfoLongPressHandlers(callback, delay = PRODUCT_INFO_LONG_PRESS_DURATION) {
+    let timer = null;
+
+    function startLongPress(params) {
+        timer = setTimeout(() => {
+            callback(params);
+        }, delay);
+    }
+
+    function cancelLongPress() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    }
+
+    return {
+        onMouseDown(event, params) {
+            if (event.button === 0) {
+                startLongPress(params);
+            }
+        },
+        onMouseUp: cancelLongPress,
+        onTouchStart: startLongPress,
+        onTouchEnd: cancelLongPress,
+        onScroll: cancelLongPress,
+    };
+}
 
 patch(ProductScreen.prototype, {
+    setup() {
+        super.setup?.();
+        this.longPressHandlers = createProductInfoLongPressHandlers((product) =>
+            this.pos.onProductInfoClick(product)
+        );
+        this.onScroll = debounce(this.longPressHandlers.onScroll, 200, { leading: true });
+    },
+
     getPosCustomProductCardClass(product) {
         const focusClass = "pos-custom-orderline-selected";
         const selectedProductTemplateId = this.pos.posCustomSelectedProductTemplateId;
